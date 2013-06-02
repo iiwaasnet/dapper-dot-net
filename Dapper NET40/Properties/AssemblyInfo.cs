ee3e9660e4caf53e1210d2ac69aefcf28c9ed6bb
iiwaasnet <givasena@gmail.com>
2013-06-02 22:17:45 +0200

    - NuGet spec created

--- Dapper NET40/Properties/AssemblyInfo.cs
+++ Dapper NET40/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dapper")]
+[assembly: AssemblyTitle("Dapper.Fork")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Stack Exchange")]
-[assembly: AssemblyProduct("Dapper")]
-[assembly: AssemblyCopyright("Copyright © Sam Saffron 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dapper.Fork")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -21,7 +21,7 @@ using System;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59080aa9-fa65-438f-ad2e-772d840effa9")]
+[assembly: Guid("7979e6f4-21b3-43ec-8ff8-ab1d5913bb71")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,5 +33,5 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.12.1.1")]
-[assembly: AssemblyFileVersion("1.12.1.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
